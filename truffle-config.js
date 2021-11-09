88033db7e289fcfb72850a46848ab77a8af7457e
Christian M <chriamue@gmail.com>
2021-11-09 07:57:19 +0100

    added clover network to truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 require("babel-register");
 require("babel-polyfill");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const MNEMONIC = process.env.MNEMONIC
+const MNEMONIC = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
@@ -11,23 +11,34 @@ module.exports = {
       network_id: "*", // Match any network id
     },
     kovan: {
-      provider: function(){
+      provider: function() {
         return new HDWalletProvider(
           MNEMONIC,
           `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-        )
+        );
       },
       gas: 5000000,
       gasPrice: 25000000000,
-      network_id: 42
+      network_id: 42,
     },
     okt: {
-      provider: () => new HDWalletProvider(MNEMONIC, `wss://exchaintestws.okex.org:8443`),
+      provider: () =>
+        new HDWalletProvider(MNEMONIC, `wss://exchaintestws.okex.org:8443`),
       network_id: 65,
       confirmations: 5,
       timeoutBlocks: 2000,
-      skipDryRun: true
-    }
+      skipDryRun: true,
+    },
+    clover: {
+      provider: () =>
+        new HDWalletProvider(MNEMONIC, `https://rpc.clover.finance`),
+      network_id: 1023,
+      gas: 55000000,
+      gasPrice: 10_000_000_000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
   },
   contracts_directory: "./src/contracts/",
   contracts_build_directory: "./src/abis/",
