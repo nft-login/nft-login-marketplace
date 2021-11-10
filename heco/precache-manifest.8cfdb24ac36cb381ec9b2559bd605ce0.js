1110cb17d2f38a9b7bf7aa6967a6b7ac7ed630cd
chriamue <chriamue@users.noreply.github.com>
2021-11-10 11:11:55 +0000

    deploy: 30ffa0ceeac514cf8afb82a8132c511c4dda1034

---
+++ heco/precache-manifest.8cfdb24ac36cb381ec9b2559bd605ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f273d75bed3badb075b2",
+    "url": "/nft-login-marketplace/heco/static/css/main.20248a64.chunk.css"
+  },
+  {
+    "revision": "f273d75bed3badb075b2",
+    "url": "/nft-login-marketplace/heco/static/js/main.f273d75b.chunk.js"
+  },
+  {
+    "revision": "c17e8fed9ee99496d953",
+    "url": "/nft-login-marketplace/heco/static/js/1.c17e8fed.chunk.js"
+  },
+  {
+    "revision": "b152a6d898d7e04ea9d1",
+    "url": "/nft-login-marketplace/heco/static/js/runtime~main.b152a6d8.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/heco/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/heco/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "3721f0ea61990c1c0c38578a5dbf1b9f",
+    "url": "/nft-login-marketplace/heco/index.html"
+  }
+];
---
+++ heco/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nft-login-marketplace/heco/precache-manifest.8cfdb24ac36cb381ec9b2559bd605ce0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nft-login-marketplace/heco/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
