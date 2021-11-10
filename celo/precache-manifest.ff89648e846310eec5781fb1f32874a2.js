4cc289144be33807cc22e2321459c17017091373
chriamue <chriamue@users.noreply.github.com>
2021-11-10 20:43:41 +0000

    deploy: 8dc66c7daf7ebb4467816822ace6f22db56425dc

---
+++ celo/precache-manifest.ff89648e846310eec5781fb1f32874a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d686a116987d15f114fc",
+    "url": "/nft-login-marketplace/celo/static/css/main.20248a64.chunk.css"
+  },
+  {
+    "revision": "d686a116987d15f114fc",
+    "url": "/nft-login-marketplace/celo/static/js/main.d686a116.chunk.js"
+  },
+  {
+    "revision": "ac803d9498d7317d27b9",
+    "url": "/nft-login-marketplace/celo/static/js/1.ac803d94.chunk.js"
+  },
+  {
+    "revision": "39e32f274cd5475d0fcf",
+    "url": "/nft-login-marketplace/celo/static/js/runtime~main.39e32f27.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/celo/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/celo/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "2a1d51b854ee22e95958797b7502bd34",
+    "url": "/nft-login-marketplace/celo/index.html"
+  }
+];
