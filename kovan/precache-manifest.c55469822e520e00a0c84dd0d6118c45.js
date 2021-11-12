f05105feaf60709e24ea4a6442cfc4b9b46730a0
chriamue <chriamue@users.noreply.github.com>
2021-11-12 06:48:25 +0000

    deploy: ed2d4b9dd7a691db67185643f4a6c64064a3e405

---
+++ kovan/precache-manifest.c55469822e520e00a0c84dd0d6118c45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0bbd54b85d3a64efdf0",
+    "url": "/nft-login-marketplace/kovan/static/css/main.20248a64.chunk.css"
+  },
+  {
+    "revision": "a0bbd54b85d3a64efdf0",
+    "url": "/nft-login-marketplace/kovan/static/js/main.a0bbd54b.chunk.js"
+  },
+  {
+    "revision": "6b4fa0d5366b8f0a997f",
+    "url": "/nft-login-marketplace/kovan/static/js/1.6b4fa0d5.chunk.js"
+  },
+  {
+    "revision": "6cc42b20b76c1cbc24f6",
+    "url": "/nft-login-marketplace/kovan/static/js/runtime~main.6cc42b20.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/kovan/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/kovan/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "34719a6e9e1cf85183afc6f5b13dc8a4",
+    "url": "/nft-login-marketplace/kovan/index.html"
+  }
+];
---
+++ kovan/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/nft-login-marketplace/kovan/precache-manifest.c55469822e520e00a0c84dd0d6118c45.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nft-login-marketplace/kovan/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
