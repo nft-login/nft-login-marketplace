7c82975c599966b56425412e0f6f65515b7edaf2
chriamue <chriamue@users.noreply.github.com>
2021-11-13 08:09:25 +0000

    deploy: 30661e119ff0ead3a676c5de1618d1efa2c14f81

---
+++ polygon/precache-manifest.eb366ec34291849437f760e1b09f5a80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32fa74b0cdc96f1d081",
+    "url": "/nft-login-marketplace/polygon/static/css/main.20248a64.chunk.css"
+  },
+  {
+    "revision": "c32fa74b0cdc96f1d081",
+    "url": "/nft-login-marketplace/polygon/static/js/main.c32fa74b.chunk.js"
+  },
+  {
+    "revision": "5fd094c39d87cfbd2007",
+    "url": "/nft-login-marketplace/polygon/static/js/1.5fd094c3.chunk.js"
+  },
+  {
+    "revision": "894197d0f889bddd2bfe",
+    "url": "/nft-login-marketplace/polygon/static/js/runtime~main.894197d0.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/polygon/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/polygon/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "09290aebb50bc932c645532ca3b705b0",
+    "url": "/nft-login-marketplace/polygon/index.html"
+  }
+];
