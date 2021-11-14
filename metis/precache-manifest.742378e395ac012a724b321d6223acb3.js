b383230b07a73fae929341f0d59a23d8b6be0fe4
chriamue <chriamue@users.noreply.github.com>
2021-11-14 09:15:20 +0000

    deploy: da2e2c8901dc7d2a6f7ccdbdf726f80eaf022d00

---
+++ metis/precache-manifest.742378e395ac012a724b321d6223acb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcd56eed3ffbee6861d3",
+    "url": "/nft-login-marketplace/metis/static/css/main.20248a64.chunk.css"
+  },
+  {
+    "revision": "fcd56eed3ffbee6861d3",
+    "url": "/nft-login-marketplace/metis/static/js/main.fcd56eed.chunk.js"
+  },
+  {
+    "revision": "74e8e7afcace7d2ca4d0",
+    "url": "/nft-login-marketplace/metis/static/js/1.74e8e7af.chunk.js"
+  },
+  {
+    "revision": "38dcfa7ddf7757954d78",
+    "url": "/nft-login-marketplace/metis/static/js/runtime~main.38dcfa7d.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/metis/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/metis/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "f4d40676ca6112786a703376224abdd0",
+    "url": "/nft-login-marketplace/metis/index.html"
+  }
+];
