cb26e99971e05db796357506d1d7ce91a5890e46
chriamue <chriamue@users.noreply.github.com>
2021-11-15 14:43:35 +0000

    deploy: 9dd62051b638d32f5398b6a102d9b08b513157a8

---
+++ lisk/precache-manifest.8bbf24fd6373696e5c1c923e16841ea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "753bb68704ebd8a7348b",
+    "url": "/nft-login-marketplace/lisk/static/css/main.f544bec0.chunk.css"
+  },
+  {
+    "revision": "753bb68704ebd8a7348b",
+    "url": "/nft-login-marketplace/lisk/static/js/main.753bb687.chunk.js"
+  },
+  {
+    "revision": "815f4098b059fdbed30b",
+    "url": "/nft-login-marketplace/lisk/static/js/1.815f4098.chunk.js"
+  },
+  {
+    "revision": "1e89592cba4c0a33b2d0",
+    "url": "/nft-login-marketplace/lisk/static/js/runtime~main.1e89592c.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/lisk/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/lisk/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "9ab79ccd49d42c3b997c8256fadcbeed",
+    "url": "/nft-login-marketplace/lisk/index.html"
+  }
+];
--- lisk/service-worker.js
+++ lisk/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nft-login-marketplace/lisk/precache-manifest.93ea8e259af36fd03b92dc73a6489cf4.js"
+  "/nft-login-marketplace/lisk/precache-manifest.8bbf24fd6373696e5c1c923e16841ea4.js"
 );
 
 workbox.clientsClaim();
