39d37199794f50691659bf6cdc4756511c40d960
chriamue <chriamue@users.noreply.github.com>
2021-11-16 20:07:25 +0000

    deploy: 88b0bb9ae6f76f8834665568778792acc29ca981

---
+++ heco/precache-manifest.c2064ac932f6af40e224517f0d31f6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "91850dd57119b81d0142",
+    "url": "/nft-login-marketplace/heco/static/css/main.20248a64.chunk.css"
+  },
+  {
+    "revision": "91850dd57119b81d0142",
+    "url": "/nft-login-marketplace/heco/static/js/main.91850dd5.chunk.js"
+  },
+  {
+    "revision": "2c8a1b984b5827eaf748",
+    "url": "/nft-login-marketplace/heco/static/js/1.2c8a1b98.chunk.js"
+  },
+  {
+    "revision": "b152a6d898d7e04ea9d1",
+    "url": "/nft-login-marketplace/heco/static/js/runtime~main.b152a6d8.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/nft-login-marketplace/heco/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/nft-login-marketplace/heco/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "19f1b7d5a67b653679063e9d097426d2",
+    "url": "/nft-login-marketplace/heco/index.html"
+  }
+];
